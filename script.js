5379aabbb91177aca132e3b424af580aeb421f82
Bhavya <bhavya1805@gmail.com>
2024-06-18 11:24:52 -0600

    removed unused mag stuff

--- script.js
+++ script.js
@@ -69,7 +69,6 @@ function changeEpsilon() {
 
 // Turns the mag box on and off
 function turnMagOnOff() {
-  const magCanvas = document.getElementById('mag_viewer');
   const bool = parseInt(document.getElementById("magOnOffInput").value);
   if (bool) {
     MAG_LIST.forEach(MAG => MAG.calculate = true);
