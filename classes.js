a5e6f998c8617e5effee1f5bda449fe19562116d
Bhavya <bhavya1805@gmail.com>
2024-06-25 14:22:44 -0600

    Bug fix

--- classes.js
+++ classes.js
@@ -69,7 +69,7 @@ class Magnifier {
   }
 
   // Mark the center of the magnification box.
-  drawCenter(color = MAG_POINT_COLOR, radius = 3) {
+  drawCenter(color = MAG_POINT_COLOR, radius = 2) {
     if(!this.calculate) return;
     this.ctx.beginPath();
     this.ctx.fillStyle = color; 
